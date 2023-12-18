cb8fbb4ae2e34830c8ee8c004d919b71a7f27079
Sidra Fatima <fatimasidra399@gmail.com>
2023-12-18 10:53:43 +0530

    Counter App
    This is an Increment Decrement App using Vanilla Javascript, HTML and TailwindCSS

---
+++ CounterApp.js
@@ -0,0 +1,13 @@
+const countValue = document.querySelector('#Counter');
+
+const decrement = () =>{
+    let value = parseInt(countValue.innerText);
+    value = value - 1;
+    countValue.innerText = value;
+};
+
+const increment = () => {
+    let value = parseInt(countValue.innerText)
+    value = value + 1;
+    countValue.innerText = value;
+};
